880f66b0dfbcc92198796f2a1f199e92253f6df6
Daniel Snouck <dsnouck@gmail.com>
2020-01-14 14:46:26 +0100

    Add problem 2.

---
+++ ProjectEuler.Problems.Tests/Problem2Tests.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Problem2Tests.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace ProjectEuler.Problems.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    /// <summary>
+    /// Provides tests for <see cref="Problem2"/>.
+    /// </summary>
+    public class Problem2Tests
+    {
+        private readonly Problem2 problem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Problem2Tests"/> class.
+        /// </summary>
+        public Problem2Tests()
+        {
+            this.problem = new Problem2();
+        }
+
+        /// <summary>
+        /// Tests <see cref="Problem2.Solve()"/>.
+        /// </summary>
+        [Fact]
+        public void WhenSolveIsCalledThenTheCorrectAnswerIsReturned()
+        {
+            // Act.
+            var solution = this.problem.Solve();
+
+            // Assert.
+            solution.Should().Be("4613732");
+        }
+    }
+}
---
+++ ProjectEuler.Problems/Problem2.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Problem2.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace ProjectEuler.Problems
+{
+    using System.Globalization;
+
+    /// <inheritdoc/>
+    public class Problem2 : IProblem
+    {
+        /// <inheritdoc/>
+        public string Solve()
+        {
+            var previousNumber = 0;
+            var currentNumber = 1;
+            var sum = 0;
+            while (currentNumber <= 4000000)
+            {
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+
+                var nextNumber = previousNumber + currentNumber;
+                previousNumber = currentNumber;
+                currentNumber = nextNumber;
+            }
+
+            return sum.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
