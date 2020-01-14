d7bb466755f9d67263ffa96726c614d0f086dc74
Daniel Snouck <dsnouck@gmail.com>
2020-01-14 12:48:13 +0100

    Add FxCop and StyleCop.

--- ProjectEuler.Problems.Tests/UnitTest1.cs
+++ ProjectEuler.Problems.Tests/UnitTest1.cs
@@ -1,14 +1,23 @@
-using System;
-using Xunit;
+// <copyright file="UnitTest1.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
 
 namespace ProjectEuler.Problems.Tests
 {
+    using Xunit;
+
+    /// <summary>
+    /// The first unit test class.
+    /// </summary>
     public class UnitTest1
     {
+        /// <summary>
+        /// The first unit test.
+        /// </summary>
         [Fact]
         public void Test1()
         {
-
         }
     }
 }
--- ProjectEuler.Problems/Class1.cs
+++ ProjectEuler.Problems/Class1.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿// <copyright file="Class1.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
 
 namespace ProjectEuler.Problems
 {
+    /// <summary>
+    /// The first class.
+    /// </summary>
     public class Class1
     {
     }
