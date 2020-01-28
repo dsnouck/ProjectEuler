3739b58c4e2dd58e688c0f8cab3a76bfa01cf776
Daniel Snouck <dsnouck@gmail.com>
2020-01-28 10:34:51 +0100

    Add problem 3.

---
+++ ProjectEuler.Problems.Tests/Problem3Tests.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Problem3Tests.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace ProjectEuler.Problems.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    /// <summary>
+    /// Provides tests for <see cref="Problem3"/>.
+    /// </summary>
+    public class Problem3Tests
+    {
+        private readonly Problem3 problem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Problem3Tests"/> class.
+        /// </summary>
+        public Problem3Tests()
+        {
+            this.problem = new Problem3();
+        }
+
+        /// <summary>
+        /// Tests <see cref="Problem3.Solve()"/>.
+        /// </summary>
+        [Fact]
+        public void WhenSolveIsCalledThenTheCorrectAnswerIsReturned()
+        {
+            // Act.
+            var solution = this.problem.Solve();
+
+            // Assert.
+            solution.Should().Be("6857");
+        }
+    }
+}
---
+++ ProjectEuler.Problems/Problem3.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Problem3.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace ProjectEuler.Problems
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <inheritdoc/>
+    public class Problem3 : IProblem
+    {
+        /// <inheritdoc/>
+        public string Solve()
+        {
+            var number = 600851475143L;
+            var factors = new List<long>();
+            var factor = 2L;
+            while (factor <= number)
+            {
+                var nextNumber = number / factor;
+                var remainder = number - (nextNumber * factor);
+                if (remainder == 0L)
+                {
+                    factors.Add(factor);
+                    number = nextNumber;
+                }
+                else
+                {
+                    factor++;
+                }
+            }
+
+            return factors.Last().ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
