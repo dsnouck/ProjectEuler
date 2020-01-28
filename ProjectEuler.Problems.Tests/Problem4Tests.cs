a7fcb091915553ac595b351ec218201ec6b4d8a7
Daniel Snouck <dsnouck@gmail.com>
2020-01-28 11:33:21 +0100

    Add problem 4.

---
+++ ProjectEuler.Problems.Tests/Problem4Tests.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Problem4Tests.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace ProjectEuler.Problems.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    /// <summary>
+    /// Provides tests for <see cref="Problem4"/>.
+    /// </summary>
+    public class Problem4Tests
+    {
+        private readonly Problem4 problem;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Problem4Tests"/> class.
+        /// </summary>
+        public Problem4Tests()
+        {
+            this.problem = new Problem4();
+        }
+
+        /// <summary>
+        /// Tests <see cref="Problem4.Solve()"/>.
+        /// </summary>
+        [Fact]
+        public void WhenSolveIsCalledThenTheCorrectAnswerIsReturned()
+        {
+            // Act.
+            var solution = this.problem.Solve();
+
+            // Assert.
+            solution.Should().Be("906609");
+        }
+    }
+}
---
+++ ProjectEuler.Problems/Problem4.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="Problem4.cs" company="Daniel Snouck">
+// Copyright (c) Daniel Snouck. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace ProjectEuler.Problems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <inheritdoc/>
+    public class Problem4 : IProblem
+    {
+        /// <inheritdoc/>
+        public string Solve()
+        {
+            var palindromes = new List<int>();
+            for (var number = 100; number < 1000; number++)
+            {
+                for (var otherNumber = number; otherNumber < 1000; otherNumber++)
+                {
+                    var product = number * otherNumber;
+                    if (IsPalindrome(product))
+                    {
+                        palindromes.Add(product);
+                    }
+                }
+            }
+
+            return palindromes.Max().ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static bool IsPalindrome(int number, int numberBase = 10)
+        {
+            var digits = Digits(number, numberBase);
+            var reversedDigits = Enumerable.Reverse(digits).ToList();
+            return digits.SequenceEqual(reversedDigits);
+        }
+
+        private static List<int> Digits(int number, int numberBase = 10)
+        {
+            var digits = new List<int>();
+            while (number > 0)
+            {
+                var rest = number / numberBase;
+                var digit = number - (numberBase * rest);
+                digits.Add(digit);
+                number = rest;
+            }
+
+            return digits;
+        }
+    }
+}
